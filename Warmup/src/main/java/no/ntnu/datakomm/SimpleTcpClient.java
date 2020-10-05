3431a87a8ad1c0bd315707657c27d0df058e3504
Olav Valle <olavval@stud.ntnu.no>
2020-10-05 14:00:33 +0200

    Finished SimpleTCPClient class first draft. Set HOST to "datakomm.work".

--- Warmup/src/main/java/no/ntnu/datakomm/SimpleTcpClient.java
+++ Warmup/src/main/java/no/ntnu/datakomm/SimpleTcpClient.java
@@ -13,7 +13,7 @@ import java.net.Socket;
  */
 public class SimpleTcpClient {
     // Remote host where the server will be running
-    private static final String HOST = "localhost";
+    private static final String HOST = "datakomm.work";
     // TCP port
     private static final int PORT = 1301;
 
